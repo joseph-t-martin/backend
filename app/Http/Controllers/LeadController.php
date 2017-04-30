9eba217377a7800c243dc2ccc494042757b578c8
Joseph <wma_10@hotmail.com>
2017-04-30 17:11:16 +1000

    Initial commit

---
+++ app/Http/Controllers/LeadController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Lead;
+use Illuminate\Http\Request;
+
+class LeadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'email' => 'required|max:255|email',
+            'message' => 'required|max:255',
+        ]);
+
+        $lead = new Lead();
+        $lead->name = $request['name'];
+        $lead->email = $request['email'];
+        $lead->message = $request['message'];
+        $lead->save();
+
+        $result['result'] = 'success';
+        echo json_encode($result);
+        exit();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Lead  $lead
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lead $lead)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Lead  $lead
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lead $lead)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Lead  $lead
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lead $lead)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Lead  $lead
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lead $lead)
+    {
+        //
+    }
+}
